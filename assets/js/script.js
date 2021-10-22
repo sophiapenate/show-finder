e8101ca1300be91047662d1f5d634d41d95282f2
Sophia Barrett <sophia@yoursummit.media>
2021-10-22 16:11:25 -0700

    create DOM elements for event data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,100 @@
-function getShows(searchedArtist, searchedCity) {
-    var cors_preface = 'https://uofa21cors.herokuapp.com/';
-    var apiURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + searchedArtist + "&city=" + searchedCity +"&size=1&apikey=8nw8dGeQMSK25Lgn95Z3tuN9wAFfccB3";
-    fetch(cors_preface + apiURL)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        if (data._embedded) {
-            console.log(data._embedded);
+function displayShow(showObj) {
+    console.log(showObj);
+    //for (var i = 0; i < showsArr.length; i++) {
+        // create concertInfoEl
+        var concertInfoEl = document.createElement("div");
+
+        // append artist to concertInfoEl
+        var artistEl = document.createElement("p");
+        artistEl.classList = "artist";
+        artistEl.textContent = showObj.name;
+        concertInfoEl.appendChild(artistEl);
+
+        // append date to concertInfoEl
+        var dateEl = document.createElement("p");
+        dateEl.classList = "date";
+        // check if multi-day event
+        if (showObj.endDate) {
+            dateEl.textContent = showObj.startDate + " - " + showObj.endDate;
         } else {
-            console.log("No shows found.");
+            dateEl.textContent = showObj.startDate;
         }
-    })
-    .catch(function(err) {
-        console.error("ERROR: " + err);
-    })
+        concertInfoEl.appendChild(dateEl);
+
+        // append city to concertInfoEl
+        var cityEl = document.createElement("p");
+        cityEl.classList = "city";
+        cityEl.textContent = showObj.city;
+        concertInfoEl.appendChild(cityEl);
+
+        // append venue to concertInfoEl
+        var venueEl = document.createElement("p");
+        venueEl.classList = "venue";
+        venueEl.textContent = showObj.venue;
+        concertInfoEl.appendChild(venueEl);
+
+        // append concertInfoEl to DOM
+        console.log(concertInfoEl);
+   // }
+}
+
+function getShows(similarArtistsArr, searchedCity) {
+    // setup fetch function to pull data from Ticketmaster API
+    function fetchShows(artist, city) {
+        var cors_preface = 'https://uofa21cors.herokuapp.com/';
+        var apiURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + artist + "&city=" + city + "&size=1&apikey=8nw8dGeQMSK25Lgn95Z3tuN9wAFfccB3";
+        fetch(cors_preface + apiURL)
+            .then(function (response) {
+                response.json()
+                    .then(function (data) {
+                        if (data._embedded) {
+                            var fetchedEvents = data._embedded.events;
+                            for (var i = 0; i < fetchedEvents.length; i++) {
+                                // create object for each show
+                                var showObj = {};
+                                    showObj.name = fetchedEvents[i].name;
+                                    showObj.startDate = fetchedEvents[i].dates.start.localDate;
+                                    // set endDate only if it exists
+                                    if (fetchedEvents[i].dates.end) {
+                                        showObj.endDate = fetchedEvents[i].dates.end.localDate;
+                                    }
+                                    showObj.city = fetchedEvents[i]._embedded.venues[0].city.name,
+                                    showObj.venue = fetchedEvents[i]._embedded.venues[0].name,
+                                    showObj.getTixURL = fetchedEvents[i].url;
+                                // display object to DOM
+                                displayShow(showObj);
+                            }
+                        } else {
+                            console.log("No shows found.");
+                        }
+                    })
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+    }
+
+    // loop through each artist in similarArtistsArr and fetch shows for each
+    for (var i = 0; i < similarArtistsArr.length; i++) {
+        fetchShows(similarArtistsArr[i], searchedCity);
+    }
 }
 
 function getSimilarArtists(searchedTerm) {
-	var cors_preface = 'https://uofa21cors.herokuapp.com/';
-	var apiURL = "https://tastedive.com/api/similar?q=" + searchedTerm + "&k=425855-ShowFind-GMZOGDQD"
-	fetch(cors_preface + apiURL)
-	.then(function(response) {
-		return response.json();
-	})
-	.then(function(data) {
-		console.log(data);
-        var similarArtistsArr = [];
-        // write loop to push artist names into similarArtistsArr
-	})
-	.catch(function(err) {
-		console.error(err);
-	});
+    var cors_preface = 'https://uofa21cors.herokuapp.com/';
+    var apiURL = "https://tastedive.com/api/similar?q=" + searchedTerm + "&k=425855-ShowFind-GMZOGDQD"
+    fetch(cors_preface + apiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var similarArtistsArr = [];
+            // write loop to push artist names into similarArtistsArr
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
 }
 
 function searchFormHandler() {
@@ -45,16 +108,8 @@ function searchFormHandler() {
         "The White Stripes",
         "Incubus"
     ];
-    var searchedLocation = "London";
-    
-    for (var i = 0; i < similarArtistsArr.length; i++) {
-        // set timeout to avoid 5 requests per second quota policy violation
-        //setTimeout(function() {
-            console.log(similarArtistsArr);
-            console.log(similarArtistsArr[i], searchedLocation);
-            getShows(similarArtistsArr[i], searchedLocation);
-        //}, 200);
-    }
+    var searchedLocation = "Boston";
+    getShows(similarArtistsArr, searchedLocation);
 }
 
 searchFormHandler();
